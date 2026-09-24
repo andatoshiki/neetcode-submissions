1286789299b122ba43c3529179f2a63e15571eb5
Anda Toshiki <hello@toshiki.dev>
2026-09-24 01:57:49 -0700

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,16 @@
+// import java.util.HashMap;
+
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsValue(nums[i])) {
+                return true;
+            }
+            map.put(i, nums[i]);
+        }
+
+        return false;
+    }
+}
